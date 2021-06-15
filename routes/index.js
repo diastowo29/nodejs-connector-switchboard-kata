5ac217e8ae70e7906df31f0032464df0c4919b9c
Diastowo <diastowo@gmail.com>
2021-06-15 10:09:26 +0700

    json stringify

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get('/webhook', function(req, res, next) {
 
 router.post('/webhook', function(req, res, next) {
   var appId = req.body.app.id;
-  console.log(req.body)
+  console.log(JSON.stringify(req.body))
   req.body.events.forEach(event => {
     if (event.type != 'conversation:read') {
       console.log('WEBHOOK from Smooch');
