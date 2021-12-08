5bdd20ea0e4ccaff0ead00390c2469d525043521
Diastowo <diastowo@gmail.com>
2021-12-08 17:08:14 +0700

    strict logging - push to heroku

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var basicAuth = defaultClient.authentications['basicAuth'];
 var SMOOCH_KEY_ID = process.env.SMOOCH_KEY_ID;
 var SMOOCH_KEY_SECRET = process.env.SMOOCH_KEY_SECRET;
 var BYPASS_ZD  = process.env.BYPASS_ZD;
-var WA_ACTIVE_ACCOUNT;
+var WA_ACTIVE_ACCOUNT = process.env.WA_ACTIVE_ACCOUNT;
 
 basicAuth.username = SMOOCH_KEY_ID;
 basicAuth.password = SMOOCH_KEY_SECRET;
@@ -23,31 +23,36 @@ router.get('/webhook', function(req, res, next) {
 
 router.post('/webhook', function(req, res, next) {
   var appId = req.body.app.id;
-  console.log(JSON.stringify(req.body))
-  // req.body.events.forEach(event => {
-  //   if (event.type != 'conversation:read') {
-  //     console.log('WEBHOOK from Smooch');
-  //     console.log('User: ' + event.payload.message.author.displayName);
-  //     var convId = event.payload.conversation.id;
-  //     var convSwitchboardName = event.payload.conversation.activeSwitchboardIntegration.name;
-  //     console.log('Switchboard: ' + convSwitchboardName)
-  //     var convChannel = event.payload.message.source.type;
-  //     if (convSwitchboardName == 'bot') {
-  //       if (BYPASS_ZD) {
-  //         console.log('=== PASS CONTROL TO ZENDESK ===')
-  //         switchboardPassControl(appId, convId);
-  //       } else {
-  //           if (event.payload.message.author.type == "user") {
-  //             var messagePayload = event.payload.message;
-  //             var userIdForBot = messagePayload.author.userId + ':' + appId + ':' + convId;
-  //             if (messagePayload.content.type = 'text') {
-  //               sendToBot(userIdForBot, messagePayload.content.text);
-  //             }
-  //           }
-  //       }
-  //     }
-  //   }
-  // });
+  req.body.events.forEach(event => {
+    if (event.type != 'conversation:read') {
+      var convChannel = event.payload.message.source.type;
+      var convIntegrationId = event.payload.message.source.integrationId;
+      if (convChannel == 'whatsapp') {
+        if (WA_ACTIVE_ACCOUNT.includes(convIntegrationId)) {
+          console.log(JSON.stringify(req.body))
+          console.log('WEBHOOK from Smooch');
+          console.log('User: ' + event.payload.message.author.displayName);
+          var convId = event.payload.conversation.id;
+          var convSwitchboardName = event.payload.conversation.activeSwitchboardIntegration.name;
+          console.log('Switchboard: ' + convSwitchboardName)
+          if (convSwitchboardName == 'bot') {
+            if (BYPASS_ZD) {
+              console.log('=== PASS CONTROL TO ZENDESK ===')
+              switchboardPassControl(appId, convId);
+            } else {
+                if (event.payload.message.author.type == "user") {
+                  var messagePayload = event.payload.message;
+                  var userIdForBot = messagePayload.author.userId + ':' + appId + ':' + convId;
+                  if (messagePayload.content.type = 'text') {
+                    sendToBot(userIdForBot, messagePayload.content.text);
+                  }
+                }
+            }
+          }
+        }
+      }
+    }
+  });
   res.status(200).send({});
 })
 
