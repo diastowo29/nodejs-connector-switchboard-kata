a3a3e9503178f31d82cb0dc18f2f9339a847855a
Diastowo <diastowo@gmail.com>
2021-12-30 13:45:57 +0700

    fixing logs

--- routes/index.js
+++ routes/index.js
@@ -125,7 +125,7 @@ router.post('/handover', function(req, res, next) {
     winston.log('error', {
       process: 'handover', 
       status: 'error',
-      to: userId + ':' + appId + ':' + convId,
+      to: userId,
       message: req.body
     });
     res.status(400).send({
