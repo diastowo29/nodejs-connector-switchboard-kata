97e958c39b6dd3f4f7bdfed37964f03dc80a9165
Diastowo <diastowo@gmail.com>
2022-02-23 16:49:48 +0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -102,7 +102,7 @@ router.post('/webhook', function (req, res, next) {
       var convId = event.payload.conversation.id;
       var convSwitchboardName = event.payload.conversation.activeSwitchboardIntegration.name;
       console.log('inbound: ' + event.payload.message.author.displayName + ' switchboard: ' + event.payload.conversation.activeSwitchboardIntegration.name)
-      if (convChannel == 'whatsapp') {
+      // if (convChannel == 'whatsapp') {
         if ('activeSwitchboardIntegration' in event.payload.conversation) {
           // console.log('== inbound message type: ' + convChannel + ' sw name: ' + event.payload.conversation.activeSwitchboardIntegration.name 
           //             + ' integrationId: ' + convIntegrationId + ' active wa account: ' + WA_ACTIVE_ACCOUNT.includes(convIntegrationId) + ' author: ' + event.payload.message.author.displayName)
@@ -141,47 +141,47 @@ router.post('/webhook', function (req, res, next) {
             }
           }
         }
-      } else {
-        var messagePayload = event.payload.message;
-        // console.log(JSON.stringify(req.body))
-        if (convChannel == 'android' || convChannel == 'twitter') {
-          console.log('from android')
-          // if (messagePayload.content.text.includes('car123')) {
-          //   console.log('send carousel')
-          //   hcSendCarouseltoSmooch(messagePayload.author.userId, appId, convId, messagePayload)
-          // } else if (messagePayload.content.text.includes('switch')) {
-          //   switchboardPassControl(appId, convId);
-          // } else {
-          //   console.log('-- not from whatsapp, pass to zd imidiately -- ')
-          //   switchboardPassControl(appId, convId);
-          // }
-          if (convSwitchboardName == 'bot') {
-            if (BYPASS_ZD == 'true') {
-              console.log('=== Inbound Chat from:  ' + displayName + ', Pass Control to Zendesk ===')
-              switchboardPassControl(appId, convId);
-            } else {
-              if (event.payload.message.author.type == "user") {
-                var userIdForBot = messagePayload.author.userId + '_' + appId + '_' + convId;
-                console.log('=== Inbound Chat from:  ' + displayName + ', Pass to Bot ===')
-                if (messagePayload.content.type == 'text') {
-                  sendToBot(displayName, userIdForBot, messagePayload.content.text);
-                } else if (messagePayload.content.type == 'location') {
-                  sendLocationToBot(userIdForBot, messagePayload.content)
-                } else if (messagePayload.content.type == 'file') {
-                  sendFileToBot(userIdForBot, messagePayload.content);
-                } else if (messagePayload.content.type == 'image') {
-                  sendImageToBot(userIdForBot, messagePayload.content)
-                }
-              }
-            }
-          }
-        } else if (convChannel == 'api:conversations') {
-          
-        } else {
-          console.log('inbound from channel ' + convChannel)
-          switchboardPassControl(appId, convId);
-        }
-      }
+      // } else {
+      //   var messagePayload = event.payload.message;
+      //   // console.log(JSON.stringify(req.body))
+      //   if (convChannel == 'android' || convChannel == 'twitter') {
+      //     console.log('from android')
+      //     // if (messagePayload.content.text.includes('car123')) {
+      //     //   console.log('send carousel')
+      //     //   hcSendCarouseltoSmooch(messagePayload.author.userId, appId, convId, messagePayload)
+      //     // } else if (messagePayload.content.text.includes('switch')) {
+      //     //   switchboardPassControl(appId, convId);
+      //     // } else {
+      //     //   console.log('-- not from whatsapp, pass to zd imidiately -- ')
+      //     //   switchboardPassControl(appId, convId);
+      //     // }
+      //     if (convSwitchboardName == 'bot') {
+      //       if (BYPASS_ZD == 'true') {
+      //         console.log('=== Inbound Chat from:  ' + displayName + ', Pass Control to Zendesk ===')
+      //         switchboardPassControl(appId, convId);
+      //       } else {
+      //         if (event.payload.message.author.type == "user") {
+      //           var userIdForBot = messagePayload.author.userId + '_' + appId + '_' + convId;
+      //           console.log('=== Inbound Chat from:  ' + displayName + ', Pass to Bot ===')
+      //           if (messagePayload.content.type == 'text') {
+      //             sendToBot(displayName, userIdForBot, messagePayload.content.text);
+      //           } else if (messagePayload.content.type == 'location') {
+      //             sendLocationToBot(userIdForBot, messagePayload.content)
+      //           } else if (messagePayload.content.type == 'file') {
+      //             sendFileToBot(userIdForBot, messagePayload.content);
+      //           } else if (messagePayload.content.type == 'image') {
+      //             sendImageToBot(userIdForBot, messagePayload.content)
+      //           }
+      //         }
+      //       }
+      //     }
+      //   } else if (convChannel == 'api:conversations') {
+
+      //   } else {
+      //     console.log('inbound from channel ' + convChannel)
+      //     switchboardPassControl(appId, convId);
+      //   }
+      // }
     } else {
       console.log(event.type)
     }
