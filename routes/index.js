596b5eb66385da14edcbdf2dd76987fce113ec4b
JanickNorman <janicknorman@gmail.com>
2022-11-16 17:04:52 +0700

    tes hardcode

--- routes/index.js
+++ routes/index.js
@@ -477,12 +477,13 @@ function switchboardPassControl(appId, convId, solved, firstMsgId, userId = null
   passControlBody.metadata = {
     ['dataCapture.systemField.tags']: solvedTag,
     ['dataCapture.ticketField.10051072301335']: convId,
-    ['dataCapture.ticketField.10209017032855']: userId
+    ['dataCapture.ticketField.10209017032855']: userId,
+    ['dataCapture.ticketField.5140407603223']: 'outstanding_bot'
   }
 
-  Object.entries(ticket_fields).map(f => {
-    passControlBody.metadata[[`dataCapture.ticketField.${f[0]}`]] = f[1]
-  })
+  // Object.entries(ticket_fields).map(f => {
+  //   passControlBody.metadata[[`dataCapture.ticketField.${f[0]}`]] = f[1]
+  // })
   passControlBody.metadata[['first_message_id']] = firstMsgId
 
   console.log('passing control chat', passControlBody)
