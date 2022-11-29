058329f476ff01fb572f675985c8007f3666bcad
Diastowo <diastowo@gmail.com>
2022-11-29 10:24:35 +0700

    revert convId

--- routes/index.js
+++ routes/index.js
@@ -466,7 +466,7 @@ function switchboardPassControl(appId, convId, solved, firstMsgId, userId = null
   passControlBody.switchboardIntegration = 'next';
   passControlBody.metadata = {
     ['dataCapture.systemField.tags']: solvedTag,
-    ['dataCapture.ticketField.10530778827415']: convId,
+    ['dataCapture.ticketField.10051072301335']: convId,
     ['dataCapture.ticketField.10209017032855']: userId
   }
 
